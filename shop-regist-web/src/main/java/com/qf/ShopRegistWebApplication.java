6ac74df34431a5e678c2927b959bfbd1209ab709
jrax <1193435894@qq.com>
2020-03-10 15:23:40 +0800

    frist

---
+++ shop-regist-web/src/main/java/com/qf/ShopRegistWebApplication.java
@@ -0,0 +1,13 @@
+package com.qf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopRegistWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopRegistWebApplication.class, args);
+	}
+
+}
---
+++ shop-server/src/main/java/com/qf/ShopServerApplication.java
@@ -0,0 +1,13 @@
+package com.qf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopServerApplication.class, args);
+	}
+
+}
